c836b97cf9a173f5bd6fd43b7782dae3c64274da
narkrai <narkrai@gmail.com>
2014-10-30 22:34:31 +0100

    Commit
    Commit

--- src/view/IHM_GameUI.java
+++ src/view/IHM_GameUI.java
@@ -10,12 +10,21 @@ public class IHM_GameUI extends JFrame{
 
     public IHM_GameUI() {
         JFrame GameUI = new JFrame();
-        
+        GameUI.setTitle("Rogue");
+        GameUI.setSize(1000,1000);
+        GameUI.setLocationRelativeTo(null);
+        GameUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GameUI.setVisible(true);
     }
 
     public void updateComposants() {
     }
 
     public void PutComposants() {
+    }
+    
+    public static void main(String[] args) {
+        
+        IHM_GameUI gui = new IHM_GameUI();
     }
 }
