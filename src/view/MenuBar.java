25adb2882d0b9dd89c1786891a22d5dd5b1f91aa
narkrai <narkrai@gmail.com>
2014-10-31 14:12:51 +0100

    Modif MenuBar

--- src/view/MenuBar.java
+++ src/view/MenuBar.java
@@ -12,13 +12,15 @@ public class MenuBar extends JMenuBar{
         //Déclaration frames
         final IHM_Options optionFrame = new IHM_Options();
         final IHM_GameOver go = new IHM_GameOver();
+        final IHM_Exit exit = new IHM_Exit();
+        final IHM_Stairs stairs = new IHM_Stairs();
         
         // Déclaration des JMenu et JMenuItem
         JMenu game = new JMenu("Game");
         JMenu help = new JMenu("?");
         JMenuItem newGame = new JMenuItem("New game");
-        JMenuItem test = new JMenuItem("Test");
         JMenuItem option = new JMenuItem("Option");
+        
         
         // Ajoute les menu dans la barre
         this.add(game);
@@ -26,7 +28,6 @@ public class MenuBar extends JMenuBar{
         
         // Ajoute les sous-menu dans les menu
         game.add(newGame);
-        game.add(test);
         game.add(option);
         
         //Actionlistener
@@ -35,10 +36,31 @@ public class MenuBar extends JMenuBar{
                 optionFrame.setVisible(true);
             }
         });
-        test.addActionListener(new ActionListener(){
+        
+        //Menu pour tester les frames popup, à supprimer
+        JMenu TestFrame = new JMenu("Test de frame");
+        JMenuItem gom = new JMenuItem("GameOver");
+        TestFrame.add(gom);
+        this.add(TestFrame);
+        gom.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e) {
                 go.setVisible(true);
             }
         });
+        JMenuItem exm = new JMenuItem("Exit");
+        TestFrame.add(exm);
+        exm.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                exit.setVisible(true);
+            }
+        });
+        JMenuItem stm = new JMenuItem("Stairs");
+        TestFrame.add(stm);
+        stm.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                stairs.setVisible(true);
+            }
+        });
+        
     }
 }
